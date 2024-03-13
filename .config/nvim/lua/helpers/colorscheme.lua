8609cc239b266643915393aa94d599ffd6222c08
Mick Clarke <avastmick@outlook.com>
2024-03-13 15:58:42 +1300

    fix: catppuccin theme now transparent. Added simpler terminal prompt.

--- .config/nvim/lua/helpers/colorscheme.lua
+++ .config/nvim/lua/helpers/colorscheme.lua
@@ -16,27 +16,26 @@ local function get_if_available(name, opts)
 end
 
 -- Uncomment the colorscheme to use
--- Note: the correct way to configure colorscheme
+
+-- Gruvbox
 -- local colorscheme = get_if_available('gruvbox', {
 --     terminal_colors = true, -- add neovim terminal colors
 --     transparent_mode = true,
 --     dim_inactive = false,
 --     contrast = "soft", -- can be "hard", "soft" or empty string
 -- })
+
+-- Catppuccin
 local colorscheme = get_if_available('catppuccin', {
+    --                             -- light ----------------> dark
     flavour = "macchiato",         -- latte, frappe, macchiato, mocha
     transparent_background = true, -- disables setting the background color.
     background = {                 -- :h background
         light = "latte",
-        dark = "frappe",
+        dark = "macchiato",
     },
     show_end_of_buffer = true, -- shows the '~' characters after the end of buffers
     term_colors = true,        -- sets terminal colors (e.g. `g:terminal_color_0`)
-    dim_inactive = {
-        enabled = false,       -- dims the background color of inactive window
-        shade = "dark",
-        percentage = 0.1,      -- percentage of the shade to apply to the inactive window
-    },
 })
 
 return colorscheme
