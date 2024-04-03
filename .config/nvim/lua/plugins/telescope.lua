4ff6742ad6ea1eff04b89d3cba9dbff99dff0449
Mick Clarke <avastmick@outlook.com>
2024-04-03 20:02:53 +1300

    fix: telescope file browser keymap wrong dir.

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -59,7 +59,7 @@ return {
             end, "Search in current buffer")
             map("n", "<leader>fs", require("telescope.builtin").lsp_document_symbols, "Find Symbols")
 
-            map("n", "<space>fb", ":Telescope file_browser<CR>", "File browser")
+            map("n", "<space>fb", ":Telescope file_browser path=%:p:h select_buffer=true<CR>", "File browser")
 
             map("n", "<leader>sk", require("telescope.builtin").keymaps, "Search keymaps")
             map("n", "<leader>sh", require("telescope.builtin").help_tags, "Help")
