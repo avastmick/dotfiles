1fda30817ca8033ff9d1598397afe896cea3ec9c
Mick Clarke <avastmick@outlook.com>
2024-04-12 16:20:32 +1200

    fix: lualine diagnostics

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -17,19 +17,44 @@ return {
             options = {
                 icons_enabled = true,
                 theme = lualine_theme,
-                component_separators = "|",
+                component_separators = '',
                 section_separators = "",
             },
             sections = {
-                lualine_a = { "mode" },
-                lualine_b = { {
-                    'branch',
-                    icon = '',
-                    'diagnostics',
-                } },
+                lualine_a = { 'mode' },
+                lualine_b = {
+                    {
+                        'filename',
+                        path = 1,
+                    },
+                    {
+                        'branch',
+                        icon = '',
+                    },
+                },
                 lualine_c = { {
-                    'filename',
-                    path = 1,
+                    'diagnostics',
+
+                    -- Table of diagnostic sources, available sources are:
+                    --   'nvim_lsp', 'nvim_diagnostic', 'nvim_workspace_diagnostic', 'coc', 'ale', 'vim_lsp'.
+                    -- or a function that returns a table as such:
+                    --   { error=error_cnt, warn=warn_cnt, info=info_cnt, hint=hint_cnt }
+                    sources = { 'nvim_lsp' },
+
+                    -- Displays diagnostics for the defined severity types
+                    sections = { 'error', 'warn', 'info', 'hint' },
+
+                    diagnostics_color = {
+                        -- Same values as the general color option can be used here.
+                        error = 'DiagnosticError', -- Changes diagnostics' error color.
+                        warn  = 'DiagnosticWarn',  -- Changes diagnostics' warn color.
+                        info  = 'DiagnosticInfo',  -- Changes diagnostics' info color.
+                        hint  = 'DiagnosticHint',  -- Changes diagnostics' hint color.
+                    },
+                    symbols = { error = " ", warn = " ", hint = "󱩎 ", info = " " },
+                    colored = true,          -- Displays diagnostics status in color if set to true.
+                    update_in_insert = true, -- Update diagnostics in insert mode.
+                    always_visible = false,  -- Show diagnostics even if there are none.
                 } },
                 lualine_x = { "encoding", { "fileformat", symbols = { unix = os_icon } }, "filetype" },
                 lualine_y = { "progress" },
