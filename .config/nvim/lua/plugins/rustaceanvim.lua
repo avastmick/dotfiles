22709591c698edc34b51488d9fe102fc1841e68d
Mick Clarke <avastmick@outlook.com>
2024-08-07 12:04:20 +1200

    fix: moved to stow for dotfiles. Clearing out.

--- .config/nvim/lua/plugins/rustaceanvim.lua
+++
@@ -1,8 +0,0 @@
--- Rust specific configuration
--- Note that I DO NOT use the LSP for Rust.
--- See: https://github.com/mrcjkb/rustaceanvim for more details.
-return {
-    'mrcjkb/rustaceanvim',
-    version = '^4',
-    ft = { 'rust' },
-}
